0c4bb677d36a6d6f02bc0caa3ef3d1947c41c2ad
delsalle-sio-proust-a <delasalle.sio.proust.a@gmail.com>
2020-10-06 16:28:05 +0200

    suppression etape 7

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-//yo les boys ça va ou quoi ?
-//ajout d'une ligne par proust le 06/10/2020 15:58
-//Wesh tu veux pas la guerre mais pourquoi t'allume la mèche? en ligne par moi le mm jour
-//ajout d'une ligne
-?>
